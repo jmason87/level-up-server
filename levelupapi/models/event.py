2b859a9f7bda9f05410cdca8aef26e1aa1022fea
jmason87 <joewebdev87@gmail.com>
2022-01-31 15:15:45 -0600

    initial commit

---
+++ levelupapi/models/event.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Event(models.Model):
+    game = models.ForeignKey("Game", on_delete=models.CASCADE)
+    description = models.CharField(max_length=100) 
+    date = models.DateField(auto_now_add=True) 
+    time = models.TimeField(auto_now_add=True)
+    organizer = models.ForeignKey("Gamer", on_delete=models.CASCADE) 
