dd9cdab79f4844383c71be5f96b6e4c7882e6ffe
jmason87 <joewebdev87@gmail.com>
2022-02-23 10:34:39 -0600

    book 3 chapter 3

--- db.sql
+++ db.sql
@@ -1,4 +1,6 @@
-SELECT * FROM auth_user;
-SELECT * FROM authtoken_token;
-SELECT * FROM levelupapi_game;
-SELECT * FROM levelupapi_event
+SELECT GM.*, A.id, A.first_name || " " || A.last_name as "FullName"
+FROM levelupapi_game GM
+JOIN levelupapi_gamer GMR
+ON GM.gamer_id = GMR.id
+JOIN auth_user A 
+on GMR.user_id = A.id
